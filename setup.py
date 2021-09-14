a33e1ad29da941767d08ba33a2dbf9921efc65be
David Nidever <dnidever@lsst.org>
2021-09-14 17:06:18 -0600

    renaming changes

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 #from distutils.core import setup
 from setuptools import setup, find_packages
 
-setup(name='PSFPhot',
+setup(name='prometheus',
       version='1.0',
       description='PSF Photometry Software',
       author='David Nidever',
       author_email='dnidever@montana.edu',
-      url='https://github.com/dnidever/psfphot',
+      url='https://github.com/dnidever/prometheus',
       packages=find_packages(exclude=["tests"]),
 #      scripts=['bin/dopfit','bin/dopjointfit','bin/doppler'],
       requires=['numpy','astropy(>=4.0)','scipy','dlnpyutils'],
