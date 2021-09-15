7aa5a7e287c78bef4c910343d5208a5ce4eb265a
David Nidever <dnidever@lsst.org>
2021-09-15 15:09:37 -0600

    update

--- prometheus/__init__.py
+++ prometheus/__init__.py
@@ -1,2 +1,9 @@
 __all__ = ["models","getpsf","psfphot","synth","groupfit","starfit","leastsquares","ccddata","detection","aperture","sky"]
 __version__ = '1.0.0'
+
+from .ccddata import CCDData
+
+read = CCDData.read
+
+# function to run all steps
+#fit = 
