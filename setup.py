ca6e7381dba18c06d70adfe1879fde89bcee391b
David Nidever <dnidever@lsst.org>
2021-09-15 15:19:25 -0600

    updated the requirements

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(name='prometheus',
       url='https://github.com/dnidever/prometheus',
       packages=find_packages(exclude=["tests"]),
 #      scripts=['bin/dopfit','bin/dopjointfit','bin/doppler'],
-      requires=['numpy','astropy(>=4.0)','scipy','dlnpyutils'],
+      requires=['numpy','astropy(>=4.0)','scipy','dlnpyutils','sep','photutils'],
 #      include_package_data=True,
 )
