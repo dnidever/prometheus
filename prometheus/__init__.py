85d5e44d111e51d61604e8e5b080fac00df3345a
David Nidever <dnidever@lsst.org>
2021-11-05 11:53:13 -0600

    bumped version from 1.0.7 to 1.0.8

--- prometheus/__init__.py
+++ prometheus/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ["models","getpsf","synth","groupfit","leastsquares","allfit",
            "ccddata","detection","aperture","sky","prometheus","utils"]
-__version__ = '1.0.7'
+__version__ = '1.0.8'
 
 from .ccddata import CCDData
 from prometheus import prometheus as pm
