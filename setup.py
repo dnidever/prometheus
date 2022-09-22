d27ca8f4b8a7bce0cd210419df755e45f6260ae8
David Nidever <dnidever@lsst.org>
2022-09-22 16:02:30 -0600

    bumped version from 1.0.17 to 1.0.18

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 pypiname = 'theprometheus'
 
 setup(name="prometheus",
-      version='1.0.17',
+      version='1.0.18',
       description='PSF Photometry Software',
       author='David Nidever',
       author_email='dnidever@montana.edu',
