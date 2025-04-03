98e55e85a0f4def75daf89df671b2ec4ea636558
David Nidever <dnidever@montana.edu>
2025-04-02 21:20:26 -0600

    gaussian.pyx updates

--- prometheus/cython/gaussian.pyx
+++ prometheus/cython/gaussian.pyx
@@ -13,9 +13,10 @@ from cython.view cimport array as cvarray
 from cpython cimport array
 from scipy.special import gamma, gammaincinv, gammainc
 
-from libc.math cimport exp,sqrt,atan2,pi,NAN
+from libc.math cimport exp,sqrt,atan2,pi,NAN,erf
 from libcpp cimport bool
 from libc.stdlib cimport malloc, free
+from libc.stdio cimport printf
 from cpython.mem cimport PyMem_Malloc, PyMem_Realloc, PyMem_Free
 
 cdef extern from "math.h":
@@ -779,7 +780,7 @@ cdef double* gauss1d(double x, double y, double amp, double xc, double yc,
     cdef double g
     #cdef double[6] deriv = [0.0,0.0,0.0,0.0,0.0,0.0]
     #cdef double[7] out
-    #cdef double *out = <double*>malloc(7 * sizeof(double))
+    cdef double *out = <double*>malloc(7 * sizeof(double))
     #cdef double[7] out
     #cdef double amp,xc,yc,asemi,bsemi,theta,cxx,cyy,cxy
     cdef double u,u2,v,v2
@@ -803,6 +804,8 @@ cdef double* gauss1d(double x, double y, double amp, double xc, double yc,
     # amp = 1/(asemi*bsemi*2*np.pi)
     g = amp * exp(-0.5*(cxx*u**2 + cyy*v**2 + cxy*u*v))
 
+    #printf("%.3f %.3f %.3f\n",x,y,g)
+
     #out = cvarray(shape=(7,),itemsize=sizeof(double),format="d")
     #cdef double[:] mout = out
     #cdef array.array out
@@ -819,7 +822,7 @@ cdef double* gauss1d(double x, double y, double amp, double xc, double yc,
 
     #out[0] = g
 
-    cdef double[7] out = [0,0,0,0,0,0,0]
+    #cdef double[7] out = [0,0,0,0,0,0,0]
 
     dg_dA = 0.0
     dg_dx_mean = 0.0
@@ -888,6 +891,8 @@ cdef double* gauss1d(double x, double y, double amp, double xc, double yc,
     out[4] = dg_dxsig
     out[5] = dg_dysig
     out[6] = dg_dtheta
+
+    #printf("%.3f %.3f %.3f\n",out[0],out[1],out[2])
 
     #print(out)
 
@@ -967,7 +972,7 @@ cdef double* gauss2d3(double[:] x, double[:] y, double[:] pars, int nderiv):
         #gauss1d(1.0,1.0,allpars,6)
         #gauss1d(1.0,1.0,amp,xc,yc,asemi,bsemi,theta,cxx,cyy,cxy,nderiv)
         out1 = gauss1d(x1,y1,amp,xc,yc,asemi,bsemi,theta,cxx,cyy,cxy,nderiv)
-        #print(out1)
+        #printf("in gauss2d3 loop: %.3f %.3f %.3f\n",out1[0],out1[1],out1[2])
         #gauss1d(x1,y1,amp,xc,yc,asemi,bsemi,theta,cxx,cyy,cxy,nderiv,out1)
         #o1,o2,o3,o4,o5,o6,o7 = gauss1d(x1,y1,amp,xc,yc,asemi,bsemi,theta,cxx,cyy,cxy,nderiv)
         #out1 = gauss1d(x1,y1,allpars,nderiv)
@@ -995,7 +1000,9 @@ cdef double* gauss2d3(double[:] x, double[:] y, double[:] pars, int nderiv):
         out[index+4] = out1[4]
         out[index+5] = out1[5]
         out[index+6] = out1[6]
-	
+
+    #printf("end of gauss2d3: %.3f %.3f %.3f\n",out[0],out[1],out[2])
+
     #free(out)
 
     return out
@@ -1013,31 +1020,26 @@ cpdef double[:,:] testgauss2d3(double[:] x, double[:] y, double[:] pars, int nde
         #index = i*7
         for j in range(7):
             npout[i,j] = out[i*7+j]
-            print(npout[i,j])
+            #print(npout[i,j])
     free(out)
-    #PyMem_Free(out)
+    ##PyMem_Free(out)
 
     return npout
 
-cpdef double[:] gaussian2d_integrate(double x, double y, double[:] pars, int nderiv, int osamp):
-    cdef double theta,cost2,sint2
-    cdef double xsig2,ysig2,a,b,c,g,xdiff,ydiff,xdiff2,ydiff2,x0,y0,dd,dx,dy
-    cdef int nx,ny,col,row,hosamp,i
+#cpdef double[:] gaussian_integrate(double x, double y, double[:] pars, int nderiv, int osamp):
+#cdef double* gaussian_integrate(double x, double y, double[:] pars, int nderiv, int osamp):
+cdef void gaussian_integrate(double x, double y, double[:] pars, int nderiv, int osamp, double* out):
+    cdef double theta,cost2,sint2,amp
+    cdef double xsig2,ysig2,a,b,c,xdiff,ydiff,xdiff2,ydiff2,x0,y0,dx,dy
+    cdef int nx,ny,col,row,nsamp,hosamp,i
     #cdef double[:] x2,y2,xdiff,ydiff,g
     cdef double dg_dA,dg_dx_mean,dg_dy_mean,dg_dxsig,dg_dysig,dg_dtheta
     cdef double cost,sint,xsig3,ysig3,da_dxsig,db_dxsig,dc_dxsig
     cdef double da_dysig,db_dysig,dc_dysig
     cdef double da_dtheta,db_dtheta,dc_dtheta
 
-
-    #osamp2 = float(osamp)**2
-    #nx = x.size
-    #dx = (np.arange(osamp).astype(float)+1)/osamp-(1/(2*osamp))-0.5
-    #dx2 = np.tile(dx,(osamp,1))
-    #x2 = np.tile(x,(osamp,osamp,1)) + np.tile(dx2.T,(nx,1,1)).T
-    #y2 = np.tile(y,(osamp,osamp,1)) + np.tile(dx2,(nx,1,1)).T    
-    
     # pars = [amplitude, x0, y0, xsigma, ysigma, theta]
+    amp = pars[0]
     x0 = pars[1]
     y0 = pars[2]
     theta = pars[5]
@@ -1048,56 +1050,58 @@ cpdef double[:] gaussian2d_integrate(double x, double y, double[:] pars, int nde
     ysig = pars[4]
     xsig2 = pars[3] ** 2
     ysig2 = pars[4] ** 2
-    #xdiff = x2 - pars[1]
-    #ydiff = y2 - pars[2]
     a = 0.5 * ((cost2 / xsig2) + (sint2 / ysig2))
     b = 0.5 * ((sin2t / xsig2) - (sin2t / ysig2))
     c = 0.5 * ((sint2 / xsig2) + (cost2 / ysig2))
 
-    nsamp = osamp*osamp
-    cdef double[16] dxarr = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    cdef double[16] dyarr = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    if osamp==1:
-        dd = 0.0
-        dd0 = 0.0
-        dxarr[0] = 0.0
-        dyarr[0] = 0.0
-    elif osamp==2:
-        dd = 0.50
-        dd0 = -0.25
-        dxarr[0:4] = [-0.25,-0.25,
-                       0.25,0.25]
-        dyarr[0:4] = [-0.25,0.25,
-                      -0.25,0.25]
-    elif osamp==3:
-        dd = 0.3333
-        dd0 = -0.3333
-        dxarr[0:9] = [-0.3333,-0.3333,-0.3333,
-                      0.0,0.0,0.0,
-                      0.3333,0.3333,0.3333]
-        dyarr[0:9] = [-0.3333,0.0,0.3333,
-                      -0.3333,0.0,0.3333,
-                      -0.3333,0.0,0.3333]
-    elif osamp==4:
-        dd = 0.25
-        dd0 = -0.375
-        dxarr[0:16] = [-0.375,-0.375,-0.375,-0.375,
-	               -0.125,-0.125,-0.125,-0.125,
-                       0.125,0.125,0.125,0.125,
-                       0.375,0.375,0.375,0.375]
-        dyarr[0:16] = [-0.375,-0.125,0.125,0.375,
-                       -0.375,-0.125,0.125,0.375,
-                       -0.375,-0.125,0.125,0.375,
-                       -0.375,-0.125,0.125,0.375]
+    xdiff = x-x0
+    ydiff = y-y0
+    cdef double f = 0.0
+    f = exp(-((a * xdiff ** 2) + (b * xdiff * ydiff) +
+              (c * ydiff ** 2)))
+    #cdef double df_dx_mean = 0.0
+    #cdef double df_dy_mean = 0.0
+    #df_dx_mean = f * ((2. * a * xdiff) + (b * ydiff))
+    #df_dy_mean = f * ((b * xdiff) + (2. * c * ydiff))
+    #cdef double maxdiff = max(abs(df_dx_mean),abs(df_dy_mean))
+    #printf("%.3f\n",maxdiff)
+    #cdef double earg = ((a * xdiff ** 2) + (b * xdiff * ydiff) +
+    #                    (c * ydiff ** 2))
 
-    #cdef double g = 0.0
-    g = 0.0
+    # Automatically determine the oversampling
+    # These are the thresholds that daophot uses
+    # from the IRAF daophot version in
+    # noao/digiphot/daophot/daolib/profile.x
+    if osamp < 1:
+        if (f >= 0.046):
+            osamp = 4
+        elif (f >= 0.0022):
+            osamp = 3
+        elif (f >= 0.0001):
+            osamp = 2
+        elif (f >= 1.0e-10):
+            osamp = 1
+
+    nsamp = osamp*osamp
+    #cdef double[16] dxarr = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    #cdef double[16] dyarr = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    cdef double dd = 0.0
+    cdef double dd0 = 0.0
+    # dx = (np.arange(osamp).astype(float)+1)/osamp-(1/(2*osamp))-0.5
+    if osamp>1:
+        dd = 1/float(osamp)
+        dd0 = 1/(2*float(osamp))-0.5
+
+    cdef double g = 0.0
+    #cdef double g1 = 0.0
     #cdef double[7] out = [0,0,0,0,0,0,0]
-    #cdef double[6] derivative = [0,0,0,0,0,0]
-    out = cvarray(shape=(7,),itemsize=sizeof(double),format="d")
-    cdef double[:] mout = out
+    #cdef double *out =  <double*>malloc(7 * sizeof(double))
+    #out = cvarray(shape=(7,),itemsize=sizeof(double),format="d")
+    #cdef double[:] mout = out
+    #for i in range(7):
+    #    mout[i] = 0.0
     for i in range(7):
-        mout[i] = 0.0
+        out[i] = 0.0
     hosamp = osamp//2
     dg_dA = 0.0
     dg_dx_mean = 0.0
@@ -1110,29 +1114,24 @@ cpdef double[:] gaussian2d_integrate(double x, double y, double[:] pars, int nde
         row = i % osamp
         dx = col*dd+dd0
         dy = row*dd+dd0
-        #xdiff = (x+dx)-x0
-        #ydiff = (y+dy)-y0
-        #dx = (col-hosamp)*dd
-        #dy = (row-hosamp)*dd
-        xdiff = (x+dxarr[i])-x0
-        ydiff = (y+dyarr[i])-y0
+        xdiff = (x+dx)-x0
+        ydiff = (y+dy)-y0
         xdiff2 = xdiff*xdiff
         ydiff2 = ydiff*ydiff
-        g1 = pars[0] * np.exp(-((a * xdiff ** 2) + (b * xdiff * ydiff) +
-                                (c * ydiff ** 2)))
-        #print(i,g,col,row,dx,dy,dxarr[i],dyarr[i])
-        mout[0] += g1
+        g = amp * exp(-((a * xdiff ** 2) + (b * xdiff * ydiff) +
+                        (c * ydiff ** 2)))
+        out[0] += g
 
         # Compute derivative as well
         if nderiv>=1:
-            dg_dA = g1 / pars[0]
-            mout[1] += dg_dA
+            dg_dA = g / amp
+            out[1] += dg_dA
         if nderiv>=2:        
-            dg_dx_mean = g1 * ((2. * a * xdiff) + (b * ydiff))
-            mout[2] += dg_dx_mean
+            dg_dx_mean = g * ((2. * a * xdiff) + (b * ydiff))
+            out[2] += dg_dx_mean
         if nderiv>=3:
-            dg_dy_mean = g1 * ((b * xdiff) + (2. * c * ydiff))
-            mout[3] += dg_dy_mean
+            dg_dy_mean = g * ((b * xdiff) + (2. * c * ydiff))
+            out[3] += dg_dy_mean
         if nderiv>=4:
             cost = cos(theta)
             sint = sin(theta)
@@ -1140,30 +1139,178 @@ cpdef double[:] gaussian2d_integrate(double x, double y, double[:] pars, int nde
             da_dxsig = -cost2 / xsig3
             db_dxsig = -sin2t / xsig3
             dc_dxsig = -sint2 / xsig3        
-            dg_dxsig = g1 * (-(da_dxsig * xdiff2 +
+            dg_dxsig = g * (-(da_dxsig * xdiff2 +
                                    db_dxsig * xdiff * ydiff +
                                    dc_dxsig * ydiff2))
-            mout[4] += dg_dxsig
+            out[4] += dg_dxsig
         if nderiv>=5:
             ysig3 = ysig ** 3            
             da_dysig = -sint2 / ysig3
             db_dysig = sin2t / ysig3
             dc_dysig = -cost2 / ysig3        
-            dg_dysig = g1 * (-(da_dysig * xdiff2 +
+            dg_dysig = g * (-(da_dysig * xdiff2 +
                                    db_dysig * xdiff * ydiff +
                                    dc_dysig * ydiff2))
-            mout[5] += dg_dysig
+            out[5] += dg_dysig
         if nderiv>=6:
             cos2t = cos(2. * theta)            
             da_dtheta = (sint * cost * ((1. / ysig2) - (1. / xsig2)))
             db_dtheta = (cos2t / xsig2) - (cos2t / ysig2)
             dc_dtheta = -da_dtheta        
-            dg_dtheta = g1 * (-(da_dtheta * xdiff2 +
+            dg_dtheta = g * (-(da_dtheta * xdiff2 +
                                 db_dtheta * xdiff * ydiff +
                                 dc_dtheta * ydiff2))
-            mout[6] += dg_dtheta
+            out[6] += dg_dtheta
 
-    for i in range(7):
-        mout[i] /= nsamp   # take average
+    if osamp>1:
+        for i in range(nderiv+1):
+            out[i] /= nsamp   # take average
 
-    return mout
+
+#cpdef double[:,:] testgauss2d_integrate2(double[:] x, double[:] y, double[:] pars, int nderiv, int osamp):
+cpdef void testgauss2d_integrate2(double[:] x, double[:] y, double[:] pars, int nderiv, int osamp):
+#cdef void testgauss2d_integrate2(double[:] x, double[:] y, double[:] pars, int nderiv, int osamp, double* out):
+    cdef long npix,i,j
+    npix = len(x)
+    #cdef double[:,:] npout = np.zeros((npix,7),float)
+    cdef double *out =  <double*>malloc(7*npix * sizeof(double))
+    cdef double *out1 =  <double*>malloc(7 * sizeof(double))
+    for i in range(npix):
+        for j in range(7):
+             out1[j] = 0.0
+        gaussian_integrate(x[i],y[i],pars,nderiv,osamp,out1)
+        #printf("%.4f %.4f\n",out[0],out[1])
+        for j in range(7):
+            index = i*7
+            out[index+j] = out1[j]
+            #npout[i,j] = out[j]
+            #print(npout[i,j])
+    #    #print(out)
+    #    free(out)
+
+    free(out1)
+    free(out)
+
+    #cdef long npix = len(x)
+    #cdef long i,j
+    #cdef double[:,:] npout = np.zeros((npix,7),float)
+    #for i in range(npix):
+    #    #index = i*7
+    #    out = gaussian2d_integrate(x[i],y[i],pars,nderiv,4)
+    #    for j in range(7):
+    #        npout[i,j] = out[i*7+j]
+    #        #print(npout[i,j])
+    #    free(out)
+    #free(out)
+    #PyMem_Free(out)
+    
+    #return npout
+
+
+
+#cdef double* gaussian2d_integrate(double[:] x, double[:] y, double[:] pars, int nderiv):
+#cdef double[:,:] gaussian2d_integrate(double[:] x, double[:] y, double[:] pars, int nderiv):
+cdef void gaussian2d_integrate(double[:] x, double[:] y, double[:] pars, int nderiv, double* out):
+    """
+    Two dimensional Gaussian model function with x/y array inputs.
+    
+    Parameters
+    ----------
+    x : numpy array
+      Array of X-values of points for which to compute the Gaussian model.
+    y : numpy array
+      Array of Y-values of points for which to compute the Gaussian model.
+    pars : numpy array
+       Parameter list. pars = [amplitude, x0, y0, xsigma, ysigma, theta].
+         Or can include cxx, cyy, cxy at the end so they don't have to be
+         computed.
+    nderiv : int
+       The number of derivatives to return.
+
+    Returns
+    -------
+    g : numpy array
+      The Gaussian model for the input x/y values and parameters.  Always
+        returned as 1D raveled() array.
+    derivative : numpy array
+      List of derivatives of g relative to the input parameters.
+        Always 2D [Npix,Nderiv] with the 1st dimension being the x/y arrays
+        raveled() to 1D.
+
+    Example
+    -------
+
+    g,derivative = agaussian2d(x,y,pars,3)
+
+    """
+    #cdef double[:] g = np.zeros(len(x))
+    #cdef double[:,:] deriv = np.zeros((len(x),nderiv))
+    #cdef double[:] d1 = np.zeros(nderiv)
+    cdef double amp,xc,yc,asemi,bsemi,theta,cxx,cyy,cxy
+    cdef long i,j
+
+    amp = pars[0]
+    xc = pars[1]
+    yc = pars[2]
+    asemi = pars[3]
+    bsemi = pars[4]
+    theta = pars[5]
+    if len(pars)==6:
+        cxx,cyy,cxy = gauss_abt2cxy(asemi,bsemi,theta)
+    else:
+        cxx = pars[6]
+        cyy = pars[7]
+        cxy = pars[8]
+    #cdef array.array allpars = array.array('d',[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
+    #allpars = cvarray(shape=(9,),itemsize=sizeof(double),format="d")
+    cdef double allpars[9]
+    allpars[0] = amp
+    allpars[1] = xc
+    allpars[2] = yc
+    allpars[3] = asemi
+    allpars[4] = bsemi
+    allpars[5] = theta
+    allpars[6] = cxx
+    allpars[7] = cyy
+    allpars[8] = cxy
+
+    npix = len(x)
+
+    #cdef double *g = <double*>malloc(npix * sizeof(double))
+    #cdef double *deriv = <double*>malloc(6*npix * sizeof(double))
+    #cdef double *out =  <double*>malloc(7*npix * sizeof(double))
+    #out = cvarray(shape=(npix,7),itemsize=sizeof(double),format="d")
+    #cdef double[:,:] mout = out
+
+
+    #g = np.zeros(len(x),float)
+    #g = cvarray(shape=(npix,),itemsize=sizeof(double),format="d")
+    #cdef double[:] mg = g
+    #deriv = np.zeros((len(x),nderiv),float)
+    #deriv = cvarray(shape=(npix,nderiv),itemsize=sizeof(double),format="d")
+    #cdef double[:,:] mderiv = deriv
+    cdef double *out1 =  <double*>malloc(7 * sizeof(double))
+    # Loop over the points
+    for i in range(npix):
+        #g1,deriv1 = gaussian2d(x[i],y[i],allpars,nderiv)
+        #mg[i] = g1
+        gaussian_integrate(x[i],y[i],allpars,nderiv,0,out1)
+        for j in range(nderiv+1):
+            #mout[i,j] = out1[j]
+            out[i*7+j] = out1[j]
+        #if nderiv>0:
+        #    #deriv[i,:] = deriv1
+        #    for j in range(nderiv):
+        #        #mderiv[i,j] = deriv1[j]
+        #        mderiv[i,j] = out1[j+1]
+    free(out1)
+    #return mout
+
+#cpdef void testgauss2d3(double[:] x, double[:] y, double[:] pars, int nderiv):
+#cpdef double[:,:] testgauss2d3(double[:] x, double[:] y, double[:] pars, int nderiv):
+cpdef void testgauss2d_integrate(double[:] x, double[:] y, double[:] pars, int nderiv):
+
+    cdef long npix = len(x)
+    cdef double *out =  <double*>malloc(7 * npix * sizeof(double))
+    gaussian2d_integrate(x,y,pars,nderiv,out)
+    free(out)
