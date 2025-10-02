21882e59191d7c275606b21d220e7323301b1ff9
David Nidever <dnidever@montana.edu>
2025-10-02 12:14:43 -0600

    bumped version from 1.0.24 to 1.0.25

--- prometheus/__init__.py
+++ prometheus/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ["models","getpsf","synth","groupfit","leastsquares","allfit","multifit",
            "ccddata","detection","aperture","sky","prometheus","utils","galfit","forced"]
-__version__ = '1.0.24'
+__version__ = '1.0.25'
 
 from .ccddata import CCDData
 from prometheus import prometheus as pm
